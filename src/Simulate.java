ea1c4865732cca233588ebb486617b8f50a07f84
l00522239 <xiao.luo@huawei.com>
2021-09-06 19:46:37 +0800

    # RR2021071501463 最大数

--- src/Simulate.java
+++ src/Simulate.java
@@ -3,6 +3,61 @@ import java.util.*;
 
 public class Simulate {
 
+    // 179. 最大数
+    // 给定一组非负整数 nums，重新排列每个数的顺序（每个数不可拆分）使之组成一个最大的整数
+    public String largestNumber(int[] nums) {
+        int n = nums.length;
+        String[] strNums = new String[n];
+
+        for (int i = 0;i<n;i++) {
+            strNums[i] = String.valueOf(nums[i]);
+        }
+
+        Arrays.sort(strNums, (o1, o2) -> {
+            String s1 = o1 + o2;
+            String s2 = o2 + o1;
+            return s2.compareTo(s1);
+        });
+        if (strNums[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder ret = new StringBuilder();
+        for (String num : strNums) {
+            ret.append(num);
+        }
+        return ret.toString();
+    }
+
+    public String largestNumber1(int[] nums) {
+        int n = nums.length;
+        String[] strNums = new String[n];
+        // 转换成包装类型，以便传入 Comparator 对象（此处为 lambda 表达式）
+        Integer[] numsArr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            numsArr[i] = nums[i];
+        }
+
+        Arrays.sort(numsArr, (x, y) -> {
+            long sx = 10, sy = 10;
+            while (sx <= x) {
+                sx *= 10;
+            }
+            while (sy <= y) {
+                sy *= 10;
+            }
+            return (int) (-sy * x - y + sx * y + x);
+        });
+
+        if (numsArr[0] == 0) {
+            return "0";
+        }
+        StringBuilder ret = new StringBuilder();
+        for (int num : numsArr) {
+            ret.append(num);
+        }
+        return ret.toString();
+    }
+
     // 233. 数字 1 的个数
     // 按位模拟
     public int countDigitOne(int n) {
