f64fab5a016f16c9e8d475b47405e693c0b44c3d
l00522239 <xiao.luo@huawei.com>
2021-09-13 16:24:35 +0800

    #RR2021081600589 相交链表

--- src/LinkedLst.java
+++ src/LinkedLst.java
@@ -1,6 +1,70 @@
 import beans.ListNode;
 
 public class LinkedLst {
+
+    // 微软面试 1
+    // 带环的 相交链表
+    public boolean isConnected(ListNode a, ListNode b) {
+        ListNode aLoop = getLoop(a);
+        ListNode bLoop = getLoop(b);
+        // 都不带环
+        if (aLoop == null && bLoop == null) {
+            return false;  // 按不带环处理
+        }
+        // 一个有环
+        if (aLoop == null || bLoop == null) {
+            return false;
+        }
+        // 两个有环
+        ListNode t = aLoop.next;
+        while (t != aLoop) {
+            if (t == bLoop) {
+                return true;
+            }
+            t = t.next;
+        }
+        return false;
+    }
+
+    public ListNode getLoop(ListNode a) {
+        ListNode a1 = a;
+        ListNode a2 = a;
+        while (a2 != null && a1 != null) {
+            a1 = a1.next;
+            a2 = a2.next;
+            if (a2 == null) {
+                return null;
+            }
+            a2 = a2.next;
+            if (a1 == a2) {
+                return a1;
+            }
+        }
+        return null;
+    }
+
+    // 160. 相交链表
+    public ListNode getIntersectionNode(ListNode a, ListNode b) {
+        int c1 = 0, c2 = 0;
+        ListNode t1 = a, t2 = b;
+        while (t1 != null && ++c1 > 0) t1 = t1.next;
+        while (t2 != null && ++c2 > 0) t2 = t2.next;
+        int t = Math.abs(c1 - c2);
+        while (t-- > 0) {
+            if (c1 > c2) a = a.next;
+            else b = b.next;
+        }
+        while (a != null && b != null) {
+            if (a.equals(b)) {
+                return a;
+            } else {
+                a = a.next;
+                b = b.next;
+            }
+        }
+        return null;
+    }
+
     // 92
     public static ListNode reverseBetween(ListNode head, int m, int n) {
         int num = 1;
